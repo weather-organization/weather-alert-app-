1bc17e9f08da27c1cb1470f70116cc268d285d2d
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-01-01 20:57:14 +0300

    i am done with APIP

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const WeatherData = {
 
    },
 
+
 alerts: [
    {
       severityClass: "severa",
@@ -33,3 +34,39 @@ alerts: [
   }
 ]
 }
+
+
+// get the data API
+// const weatherAlrtApiUrl ="/current.json or /current.xml";
+
+// fetch (weatherAlrtApiUrl)
+// .then ((response)=>{
+//    if(!response.ok){
+//       alert("Error occured in the internet")
+//    }
+//    return response.json() // data will convert as string 
+//    alert("you have successful")
+// })
+// .then ((data)=>{
+//   console.log(data) 
+// })
+// .catch((error)=>{
+//    alert("error occured")
+// })
+const weatherAlrtApiUrl ="https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=London";
+function fetchApiUrl (){
+   return axios.get(weatherAlrtApiUrl)
+   .then ((response)=>{
+     console.log (response.data);
+
+   })
+   .catch ((error)=>{
+    console.log ("Error occurs:" );    
+   })
+}
+fetchApiUrl();
+
+
+
+
+
